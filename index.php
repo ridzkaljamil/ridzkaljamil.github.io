5e3aa5feb468d4ac5b376e813c632f17b7292a0c
ridzkaljamil <ridzkaljamil@gmail.com>
2024-11-10 04:53:57 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- My CSS -->
+     <link rel="stylesheet" href="style.css" />
+
+    <title>My Portofolio | Ridzkal Jamil</title>
+  </head>
+  <body id="home">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light shadow fixed-top"style="background-color: #adb5bd;">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold" href="#">Ridzkal Jamil</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#projects">Project</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Others
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="https://www.instagram.com/ridzkaljamiil/">Instagram</a></li>
+            <li><a class="dropdown-item" href="https://www.tiktok.com/@ridzkaljamil">TikTok</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="https://www.linkedin.com/in/ridzkaljamil/">Linkedin</a></li>
+          </ul>
+        </li>
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+     <!-- Akhir Navbar -->
+     
+     <!-- Jumbotron -->
+     <section class="jumbotron text-center">
+      <img src="img/ridzkaljamil.jpg" alt="Ridzkal Jamil" width="200" class="rounded-circle img-thumbnail">
+  <h1 class="display-4">Ridzkal Jamil</h1>
+  <p class="lead">Web Developer | Freelance Videographer & Photographer</p>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,128L60,112C120,96,240,64,360,80C480,96,600,160,720,160C840,160,960,96,1080,74.7C1200,53,1320,75,1380,85.3L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+</section>
+     <!-- Akhir Jumbotron -->
+<!-- About -->
+<section id="about">
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col">
+        <h2>About Me</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <p>Hai! Saya seorang web developer yang suka merancang dan membangun website yang keren dan fungsional. Saya senang mengutak-atik kode, membuat tampilan yang menarik, dan memastikan situs bekerja dengan mulus di berbagai perangkat. Baik itu website bisnis, portofolio, atau proyek kreatif, saya selalu berusaha menciptakan sesuatu yang nggak hanya bagus dilihat, tapi juga nyaman digunakan.</p>
+      </div>
+      <div class="col-md-4">
+        <p>Selain mengembangkan web, saya juga seorang fotografer dan videografer freelance. Seni visual adalah medium lain bagi saya untuk bercerita—baik melalui foto maupun video. Saya menangkap momen, emosi, dan kisah yang terkadang sulit diungkapkan dengan kata-kata. Dari potret hingga pemandangan, hingga proyek-proyek kreatif, saya selalu berusaha menghadirkan sudut pandang baru yang unik.</p>
+      </div>
+    </div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#efefef" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+</section>
+<!-- Akhir About -->
+
+<!-- Project -->
+<section id="projects">
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col">
+        <h2>My Projects</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 mb-3">
+      <div class="card">
+  <img src="img/projects/1.jpg" class="card-img-top" alt="projects1">
+  <div class="card-body">
+    <p class="card-text">Women Photoshoot : Ayangi Al Safitri. Dalam sesi ini, saya bekerja sama dengan Ayangi Al Safitri untuk menangkap keindahan dan keunikannya di depan kamera. Dengan vibe yang santai dan natural, Ayangi menampilkan sisi dirinya yang penuh percaya diri, dan hasilnya adalah potret yang autentik dan penuh karakter.</p>
+  </div>
+</div>
+      </div>
+      <div class="col-md-4 mb-3">
+      <div class="card">
+  <img src="img/projects/2.jpg" class="card-img-top" alt="projects2">
+  <div class="card-body">
+    <p class="card-text">Man Photoshoot: Rayhan Putra Jafandi. Sesi photoshoot kali ini bersama Rayhan Putra Jafandi menampilkan sisi maskulin dan karismatiknya. Dengan pose yang penuh percaya diri, setiap foto menggambarkan kekuatan dan karakter unik Rayhan, menciptakan potret yang stylish dan autentik.</p>
+  </div>
+</div>
+      </div>
+      <div class="col-md-4 mb-3">
+      <div class="card">
+  <img src="img/projects/3.jpg" class="card-img-top" alt="projects3">
+  <div class="card-body">
+    <p class="card-text">Women Photoshoot: Banyu Tirta Kp. Sesi kali ini bersama Banyu Tirta Kp menangkap kepribadian dan pesona alaminya di depan kamera. Dengan pendekatan yang santai dan penuh percaya diri, Banyu tampil memukau dalam setiap jepretan, menghasilkan foto yang kuat dan autentik.</p>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,224L48,229.3C96,235,192,245,288,213.3C384,181,480,107,576,106.7C672,107,768,181,864,192C960,203,1056,149,1152,122.7C1248,96,1344,96,1392,96L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+</section>
+<!-- Akhir Project -->
+
+<!-- Contact -->
+ <section id="contact">
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col">
+        <h2>Contact Me</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+      <form>
+  <div class="mb-3">
+    <label for="name" class="form-label">Nama Lengkap</label>
+    <input type="text" class="form-control" id="name" aria-describedby="name">
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control" id="email" aria-describedby="email">
+  </div>
+  <div class="mb-3">
+  <label for="pesan" class="form-label">Pesan</label>
+  <textarea class="form-control" id="pesan" rows="3"></textarea>
+</div>
+  <button type="submit" class="btn btn-primary">Kirim</button>
+</form>
+      </div>
+    </div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#adb5bd" fill-opacity="1" d="M0,256L48,250.7C96,245,192,235,288,224C384,213,480,203,576,218.7C672,235,768,277,864,288C960,299,1056,277,1152,229.3C1248,181,1344,107,1392,69.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+ </section>
+<!-- Akhir Contact -->
+
+<!-- Footer -->
+ <footer class="text-black text-center pb-3"style="background-color: #adb5bd;">
+  <p>Created by <a href="https://www.instagram.com/ridzkaljamiil/" class="text-dark fw-bold">Ridzkal Jamil</a></p>
+ </footer>
+<!-- Akhir Footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
